8acdfa62cf5f6aaf9020644a7eb407f510fd52ce
Bruno Berrehuel <bruno@homepad.ch>
2023-10-21 12:39:55 +0200

    Add mini-file - remove neotree

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,6 @@ return {
   { "nvim-pack/nvim-spectre", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
