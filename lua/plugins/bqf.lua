a266a81e2ba3c690e057cde10929f92e06a25c7c
Bruno Berrehuel <bruno@homepad.ch>
2023-11-04 16:36:56 +0100

    Add diffview & bqf

---
+++ lua/plugins/bqf.lua
@@ -0,0 +1,9 @@
+return {
+  "kevinhwang91/nvim-bqf",
+  filetype = "qf",
+  event = "VeryLazy",
+  dependencies = { "junegunn/fzf", "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    auto_resize_height = true,
+  },
+}
--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -4,9 +4,9 @@ return {
   -- Lua
   config = function()
     -- Lua
-    local actions = require "diffview.actions"
+    local actions = require("diffview.actions")
 
-    require("diffview").setup {
+    require("diffview").setup({
       diff_binaries = false, -- Show diffs for binaries
       enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
       git_cmd = { "git" }, -- The git executable followed by default args.
@@ -119,117 +119,117 @@ return {
           { "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle through available layouts." } },
           {
             "n",
-            "[x",
+            "<leader>dp",
             actions.prev_conflict,
             { desc = "In the merge-tool: jump to the previous conflict" },
           },
           {
             "n",
-            "]x",
+            "<leader>dn",
             actions.next_conflict,
             { desc = "In the merge-tool: jump to the next conflict" },
           },
           {
             "n",
-            "<leader>co",
-            actions.conflict_choose "ours",
+            "<leader>do",
+            actions.conflict_choose("ours"),
             { desc = "Choose the OURS version of a conflict" },
           },
           {
             "n",
-            "<leader>ct",
-            actions.conflict_choose "theirs",
+            "<leader>dt",
+            actions.conflict_choose("theirs"),
             { desc = "Choose the THEIRS version of a conflict" },
           },
           {
             "n",
-            "<leader>cb",
-            actions.conflict_choose "base",
+            "<leader>db",
+            actions.conflict_choose("base"),
             { desc = "Choose the BASE version of a conflict" },
           },
           {
             "n",
-            "<leader>ca",
-            actions.conflict_choose "all",
+            "<leader>da",
+            actions.conflict_choose("all"),
             { desc = "Choose all the versions of a conflict" },
           },
-          { "n", "dx", actions.conflict_choose "none", { desc = "Delete the conflict region" } },
+          { "n", "<leader>dx", actions.conflict_choose("none"), { desc = "Delete the conflict region" } },
           {
             "n",
-            "<leader>cO",
-            actions.conflict_choose_all "ours",
+            "<leader>dO",
+            actions.conflict_choose_all("ours"),
             { desc = "Choose the OURS version of a conflict for the whole file" },
           },
           {
             "n",
-            "<leader>cT",
-            actions.conflict_choose_all "theirs",
+            "<leader>dT",
+            actions.conflict_choose_all("theirs"),
             { desc = "Choose the THEIRS version of a conflict for the whole file" },
           },
           {
             "n",
-            "<leader>cB",
-            actions.conflict_choose_all "base",
+            "<leader>dB",
+            actions.conflict_choose_all("base"),
             { desc = "Choose the BASE version of a conflict for the whole file" },
           },
           {
             "n",
-            "<leader>cA",
-            actions.conflict_choose_all "all",
+            "<leader>dA",
+            actions.conflict_choose_all("all"),
             { desc = "Choose all the versions of a conflict for the whole file" },
           },
           {
             "n",
-            "dX",
-            actions.conflict_choose_all "none",
+            "<leader>dX",
+            actions.conflict_choose_all("none"),
             { desc = "Delete the conflict region for the whole file" },
           },
         },
         diff1 = {
           -- Mappings in single window diff layouts
-          { "n", "g?", actions.help { "view", "diff1" }, { desc = "Open the help panel" } },
+          { "n", "g?", actions.help({ "view", "diff1" }), { desc = "Open the help panel" } },
         },
         diff2 = {
           -- Mappings in 2-way diff layouts
-          { "n", "g?", actions.help { "view", "diff2" }, { desc = "Open the help panel" } },
+          { "n", "g?", actions.help({ "view", "diff2" }), { desc = "Open the help panel" } },
         },
         diff3 = {
           -- Mappings in 3-way diff layouts
           {
             { "n", "x" },
             "2do",
-            actions.diffget "ours",
+            actions.diffget("ours"),
             { desc = "Obtain the diff hunk from the OURS version of the file" },
           },
           {
             { "n", "x" },
             "3do",
-            actions.diffget "theirs",
+            actions.diffget("theirs"),
             { desc = "Obtain the diff hunk from the THEIRS version of the file" },
           },
-          { "n", "g?", actions.help { "view", "diff3" }, { desc = "Open the help panel" } },
+          { "n", "g?", actions.help({ "view", "diff3" }), { desc = "Open the help panel" } },
         },
         diff4 = {
           -- Mappings in 4-way diff layouts
           {
             { "n", "x" },
             "1do",
-            actions.diffget "base",
+            actions.diffget("base"),
             { desc = "Obtain the diff hunk from the BASE version of the file" },
           },
           {
             { "n", "x" },
             "2do",
-            actions.diffget "ours",
+            actions.diffget("ours"),
             { desc = "Obtain the diff hunk from the OURS version of the file" },
           },
           {
             { "n", "x" },
             "3do",
-            actions.diffget "theirs",
+            actions.diffget("theirs"),
             { desc = "Obtain the diff hunk from the THEIRS version of the file" },
           },
-          { "n", "g?", actions.help { "view", "diff4" }, { desc = "Open the help panel" } },
+          { "n", "g?", actions.help({ "view", "diff4" }), { desc = "Open the help panel" } },
         },
         file_panel = {
           {
@@ -341,35 +341,35 @@ return {
           { "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle available layouts" } },
           { "n", "[x", actions.prev_conflict, { desc = "Go to the previous conflict" } },
           { "n", "]x", actions.next_conflict, { desc = "Go to the next conflict" } },
-          { "n", "g?", actions.help "file_panel", { desc = "Open the help panel" } },
+          { "n", "g?", actions.help("file_panel"), { desc = "Open the help panel" } },
           {
             "n",
             "<leader>cO",
-            actions.conflict_choose_all "ours",
+            actions.conflict_choose_all("ours"),
             { desc = "Choose the OURS version of a conflict for the whole file" },
           },
           {
             "n",
             "<leader>cT",
-            actions.conflict_choose_all "theirs",
+            actions.conflict_choose_all("theirs"),
             { desc = "Choose the THEIRS version of a conflict for the whole file" },
           },
           {
             "n",
             "<leader>cB",
-            actions.conflict_choose_all "base",
+            actions.conflict_choose_all("base"),
             { desc = "Choose the BASE version of a conflict for the whole file" },
           },
           {
             "n",
             "<leader>cA",
-            actions.conflict_choose_all "all",
+            actions.conflict_choose_all("all"),
             { desc = "Choose all the versions of a conflict for the whole file" },
           },
           {
             "n",
             "dX",
-            actions.conflict_choose_all "none",
+            actions.conflict_choose_all("none"),
             { desc = "Delete the conflict region for the whole file" },
           },
         },
@@ -452,18 +452,18 @@ return {
           { "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
           { "n", "<leader>b", actions.toggle_files, { desc = "Toggle the file panel" } },
           { "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle available layouts" } },
-          { "n", "g?", actions.help "file_history_panel", { desc = "Open the help panel" } },
+          { "n", "g?", actions.help("file_history_panel"), { desc = "Open the help panel" } },
         },
         option_panel = {
           { "n", "<tab>", actions.select_entry, { desc = "Change the current option" } },
           { "n", "q", actions.close, { desc = "Close the panel" } },
-          { "n", "g?", actions.help "option_panel", { desc = "Open the help panel" } },
+          { "n", "g?", actions.help("option_panel"), { desc = "Open the help panel" } },
         },
         help_panel = {
           { "n", "q", actions.close, { desc = "Close help menu" } },
           { "n", "<esc>", actions.close, { desc = "Close help menu" } },
         },
       },
-    }
+    })
   end,
 }
