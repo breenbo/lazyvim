97f0fadff291daf1ffec2a62b33b974e5937b15b
Bruno Berrehuel <bruno@homepad.ch>
2024-04-06 10:19:35 +0200

    Add git fugitive keybindings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,11 @@
 return {
   "tpope/vim-fugitive",
   cmd = { "Git", "GBrowse", "Gdiffsplit" },
+  keys = {
+    { "<leader>gs", "<cmd>G<cr>", desc = "Git status" },
+    { "<leader>gb", "<cmd>G blame<cr>", desc = "Git blame" },
+    { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff" },
+    { "<leader>gC", "<cmd>G commit<cr>", desc = "Git commit" },
+    { "<leader>ga", "<cmd>G add .<cr>", desc = "Git add" },
+  },
 }
