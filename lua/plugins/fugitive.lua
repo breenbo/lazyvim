d3251d9923e9b062dc41368d2af0613043d57e03
Bruno Berrehuel <bruno@homepad.ch>
2024-04-06 10:28:12 +0200

    Change fugitive keys

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,11 +2,11 @@ return {
   "tpope/vim-fugitive",
   cmd = { "Git", "GBrowse", "Gdiffsplit" },
   keys = {
-    { "<leader>gs", "<cmd>G<cr>", desc = "Git status" },
+    { "<leader>ga", "<cmd>G add .<cr>", desc = "Git add" },
+    { "<leader>gS", "<cmd>G<cr>", desc = "Git status" },
     { "<leader>gb", "<cmd>G blame<cr>", desc = "Git blame" },
     { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff" },
     { "<leader>gC", "<cmd>G commit<cr>", desc = "Git commit" },
-    { "<leader>ga", "<cmd>G add .<cr>", desc = "Git add" },
-    { "<leader>gp", "<cmd>G push<cr>", desc = "Git add" },
+    { "<leader>gP", "<cmd>G push<cr>", desc = "Git push" },
   },
 }
