8a9c2eeb0c6a9ea1ae8b45cd3975a9141e4c8616
Bruno Berrehuel <bruno@homepad.ch>
2024-04-24 10:13:54 +0200

    Change wezterm colorcheme with nvim

--- lua/plugins/gen.lua
+++ lua/plugins/gen.lua
@@ -3,10 +3,11 @@ return {
   "David-Kunz/gen.nvim",
   config = function()
     require("gen").prompts["Add docstring"] = {
-      prompt = "Add docstring in javascript format to the following function:$text. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+      prompt = "Add docstring in javascript format to the following function: $register. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
     }
     require("gen").prompts["Add test"] = {
-      prompt = "Add tests function using vitest for the following function:$text. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+      prompt = "Add tests using vitest for the following function: $register. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+      extract = "```$filetype\n(.-)```",
     }
 
     --
@@ -33,15 +34,15 @@ return {
     --
     vim.keymap.set("n", "<leader>aa", "<cmd>Gen Ask<CR>", { desc = "General AI ask" })
     vim.keymap.set("n", "<leader>ag", "<cmd>Gen Generate<CR>", { desc = "Generate code and paste it" })
-    vim.keymap.set("v", "<leader>ad", "<cmd>Gen Add docstring<CR>", { desc = "Add docstring to function" })
+    vim.keymap.set("n", "<leader>ad", "<cmd>Gen Add docstring<CR>", { desc = "Add docstring to function" })
     vim.keymap.set(
       "v",
       "<leader>ac",
       "<cmd>Gen Change_Code<CR>",
       { desc = "Change code with instruction and paste it" }
     )
-    vim.keymap.set("v", "<leader>ae", "<cmd>Gen Enhance_Code<CR>", { desc = "Rewrite code with instruction" })
-    vim.keymap.set("v", "<leader>ar", "<cmd>Gen Review_Code<CR>", { desc = "Review the code" })
-    vim.keymap.set("v", "<leader>at", "<cmd>Gen Add test<CR>", { desc = "Add test" })
+    -- vim.keymap.set("v", "<leader>ae", "<cmd>Gen Enhance_Code<CR>", { desc = "Rewrite code with instruction" })
+    -- vim.keymap.set("v", "<leader>ar", "<cmd>Gen Review_Code<CR>", { desc = "Review the code" })
+    vim.keymap.set("n", "<leader>at", "<cmd>Gen Add test<CR>", { desc = "Add test" })
   end,
 }
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+-- change some telescope options and a keymap to browse plugin files
+return {
+  "nvim-telescope/telescope.nvim",
+  -- change some options
+  opts = {
+    defaults = {
+      path_display = { "truncate" },
+    },
+  },
+}
