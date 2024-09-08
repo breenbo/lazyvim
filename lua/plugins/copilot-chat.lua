4385720a6f872b646c9e80278af13419174dc1e9
Bruno Berrehuel <bruno@homepad.ch>
2024-09-08 10:47:06 +0200

    Configure copilot not in cmd completion

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,13 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  opts = function()
+    return {
+      -- options
+      prompts = {
+        Translate = {
+          prompt = "Please translate this text in german, spanish, italian and dutch",
+        },
+      },
+    }
+  end,
+}
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,51 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = false,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "<C-k>",
+          jump_next = "<C-j>",
+          accept = "<C-l>",
+          refresh = "gr",
+          open = "<C-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.3,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-j>",
+          prev = "<C-k>",
+          dismiss = "<C-m>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = "node", -- Node.js version must be > 18.x
+      server_opts_overrides = {},
+    })
+  end,
+}
